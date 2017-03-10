2da128ae9eaede88c032b6bab4cad49e40bc267c
sangurodda481 <sangurodda.481@gmail.com>
2017-03-10 19:56:04 +0530

    Update contact.php

--- contact.php
+++ contact.php
@@ -3,7 +3,7 @@
 <html lang="en">
 	<head>
 		<meta charset="utf-8">
-		<title>LiquidApt: Contact</title>
+		<title>Ilkal Granite: Contact</title>
 		<?php include "includes/head.php"; ?>
 	</head>
 
